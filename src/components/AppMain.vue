71355d56ee0b217d76ada94512be4d565f3003ac
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-25 17:29:09 +0200

    Add: milestone 5 completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,31 @@
 <template>
     <main class="flex-grow-1">
-        <div class="container">
-        Main
+        <div class="container py-3">
+            <div class="row gy-3">
+                <div class="col col-md-4" v-for="(project, index) in info" :key="index">
+                    <CardComponent :detail="project"/>
+                </div>
+            </div>
         </div>
     </main>
 </template>
 
 <script>
+import CardComponent from './utils/CardComponent.vue';
+
+
+
+
     export default {
-        name:'AppMain',
-        props: {
-            info: {
-                type: Array,
-                required: true,
-            }
+    name: "AppMain",
+    props: {
+        info: {
+            type: Array,
+            required: true,
         }
-    }
+    },
+    components: { CardComponent, },
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/utils/CardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card h-100">
+    <img src="..." class="card-img-top" alt="..." v-if="detail.image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ detail.title }}</h5>
+      <p class="card-text">
+        {{ detail.summary.substring(0, 250) }}
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    detail: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
